b16e45a666c5ca6de1282b186cac6d74d1f077d7
frankgraave <frankgraave@gmail.com>
2019-01-15 10:52:07 +0100

    work in progress
    Signed-off-by: frankgraave <frankgraave@gmail.com>

--- src/renderer/components/AccountsOverview.vue
+++ src/renderer/components/AccountsOverview.vue
@@ -21,7 +21,7 @@
             <label class="label">Choose account</label>
             <div class="field">
               <input class="is-checkradio" id="exampleRadioInline1" type="radio" name="exampleRadioInline" checked="checked">
-              <label for="exampleRadioInline1">{{ accountOneName }}</label>
+              <label for="exampleRadioInline1">{{ accountOneName }} <em>({{ jiraUrl }})</em></label>
               <br>
               <input class="is-checkradio" id="exampleRadioInline2" type="radio" name="exampleRadioInline">
               <label for="exampleRadioInline2">Option 2</label>
